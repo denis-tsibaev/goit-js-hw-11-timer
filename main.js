838b1bcab064f87e65e795cc63505a447b00dc4a
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-17 00:17:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-new CountdownTimer({
-	selector: '#timer-1',
-	targetDate: new Date('Jul 22, 2021'),
-  });
+// new CountdownTimer({
+// 	selector: '#timer-1',
+// 	targetDate: new Date('Jul 22, 2021'),
+//   });
 
-  const time = targetDate - Date.now();
+function pad(value) {
+	return String(value).padStart(2, '0');
+}
 
- // Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
+function getTimeComponents(time) {
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-
+return {days, hours, mins, secs};
+}
 
 const timer = {
-	start() {
-		const startTime = Date.now();
-		setInterval(()=> {
-console.log('qweqwe')
-		}, 1000);
-	},
+    start() {
+		const targetDate = new Date('08:00 Jun 17 2021');
+        setInterval(() => {
+            const currentTime = Date.now();
+			const deltaTime = targetDate-currentTime;
+			const {days, hours, mins, secs} = getTimeComponents(deltaTime);
+			const time = getTimeComponents(deltaTime);
+
+			updateClockface(time);
+
+			console.log(`${days}:${hours}:${mins}:${secs}`);
+        }, 1000);
+    },
 };
+
+// timer.start();
+
+const refs = {
+	spanDays: document.querySelector('[data-value="days"]'),
+	spanHours: document.querySelector('[data-value="hours"]'),
+	spanMins: document.querySelector('[data-value="mins"]'),
+	spanSecs: document.querySelector('[data-value="secs"]'),
+}
+
+function updateClockface({days, hours, mins, secs}) {
+	refs.textContent = `${days}:${hours}:${mins}:${secs}`;
+}

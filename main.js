8da789bda7cf02619cda27fcf97169a89cc72493
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-20 11:40:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,43 +4,51 @@
 //   });
 
 function pad(value) {
-	return String(value).padStart(2, '0');
+    return String(value).padStart(2, '0');
 }
 
 function getTimeComponents(time) {
-const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-return {days, hours, mins, secs};
+    return { days, hours, mins, secs };
 }
 
 const timer = {
     start() {
-		const targetDate = new Date('08:00 Jun 17 2021');
+        const targetDate = new Date('08:00 Jul 05 2021');
         setInterval(() => {
             const currentTime = Date.now();
-			const deltaTime = targetDate-currentTime;
-			const {days, hours, mins, secs} = getTimeComponents(deltaTime);
-			const time = getTimeComponents(deltaTime);
+            const deltaTime = targetDate - currentTime;
+            const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+            const time = getTimeComponents(deltaTime);
 
-			updateClockface(time);
+            updateClockface(time);
 
-			console.log(`${days}:${hours}:${mins}:${secs}`);
+            console.log(`${days}:${hours}:${mins}:${secs}`);
         }, 1000);
     },
 };
 
-// timer.start();
+timer.start();
 
 const refs = {
-	spanDays: document.querySelector('[data-value="days"]'),
-	spanHours: document.querySelector('[data-value="hours"]'),
-	spanMins: document.querySelector('[data-value="mins"]'),
-	spanSecs: document.querySelector('[data-value="secs"]'),
-}
+    spanDays: document.querySelector('[data-value="days"]'),
+    spanHours: document.querySelector('[data-value="hours"]'),
+    spanMins: document.querySelector('[data-value="mins"]'),
+    spanSecs: document.querySelector('[data-value="secs"]'),
+};
 
-function updateClockface({days, hours, mins, secs}) {
-	refs.textContent = `${days}:${hours}:${mins}:${secs}`;
+function updateClockface(
+    { spanDays, spanHours, spanMins, spanSecs },
+    { days, hours, mins, secs },
+) {
+    spanDays.textContent = `${days}`;
+    spanHours.textContent = `${hours}`;
+    spanMins.textContent = `${mins}`;
+    spanSecs.textContent = `${secs}`;
 }

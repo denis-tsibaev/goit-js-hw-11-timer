bb5c9dfd79c02635c2337ee05bc48b9de1482214
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-26 18:08:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,72 @@
 class CountdownTimer {
-    constructor({ selector, targetDate, onTick }) {
+    constructor({ selector, targetDate }) {
         this.selector = selector;
         this.targetDate = targetDate;
-        this.onTick = onTick;
+        this.start();
+    }
+
+    useMarkup() {
+        const selectorELement = document.querySelector(this.selector);
+
+        const spanDays = selectorELement.querySelector('[data-value="days"]');
+        const spanHours = selectorELement.querySelector('[data-value="hours"]');
+        const spanMins = selectorELement.querySelector('[data-value="mins"]');
+        const spanSecs = selectorELement.querySelector('[data-value="secs"]');
+
+        return { spanDays, spanHours, spanMins, spanSecs };
+    }
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(
+            Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        );
+        const mins = this.pad(
+            Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+        );
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return { days, hours, mins, secs };
     }
 
     start() {
-        const targetDate = new Date('08:00 Jul 05 2021');
+        const targetTime = this.targetDate.getTime();
+        const markup = this.useMarkup();
         const headline = document.querySelector('.headline');
-        headline.textContent = `до ${targetDate} осталось`;
+        headline.textContent = `до ${
+            this.targetDate.getDate() +
+            '-' +
+            (this.targetDate.getMonth() + 1) +
+            '-' +
+            this.targetDate.getFullYear()
+        } осталось`;
 
         setInterval(() => {
             const currentTime = Date.now();
-            const deltaTime = targetDate - currentTime;
-            const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
-            const time = this.getTimeComponents(deltaTime);
+            const deltaTime = targetTime - currentTime;
+            const deadlineTime = this.getTimeComponents(deltaTime);
+            this.updateClockface(markup, deadlineTime);
 
-            this.onTick(time, refs);
-
-            console.log(`${days}:${hours}:${mins}:${secs}`);
+            console.log(deadlineTime);
         }, 1000);
     }
 
-	getTimeComponents(time) {
-		const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-		const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-		const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-		const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-	
-		return { days, hours, mins, secs };
-	}
-
-	pad(value) {
-		return String(value).padStart(2, '0');
-	}
-	
+    updateClockface(
+        { spanDays, spanHours, spanMins, spanSecs },
+        { days, hours, mins, secs },
+    ) {
+        spanDays.textContent = `${days}`;
+        spanHours.textContent = `${hours}`;
+        spanMins.textContent = `${mins}`;
+        spanSecs.textContent = `${secs}`;
+    }
 }
 
 const timer = new CountdownTimer({
-    onTick: updateClockface,
+    selector: '#timer-1',
+    targetDate: new Date('July 05, 2021'),
 });
-
-timer.start();
-
-const refs = {
-    spanDays: document.querySelector('[data-value="days"]'),
-    spanHours: document.querySelector('[data-value="hours"]'),
-    spanMins: document.querySelector('[data-value="mins"]'),
-    spanSecs: document.querySelector('[data-value="secs"]'),
-
-	selector: document.querySelector('#timer-1'),
-};
-
-
-function updateClockface(
-    { spanDays, spanHours, spanMins, spanSecs },
-    { days, hours, mins, secs },
-) {
-    spanDays.textContent = `${days}`;
-    spanHours.textContent = `${hours}`;
-    spanMins.textContent = `${mins}`;
-    spanSecs.textContent = `${secs}`;
-}
